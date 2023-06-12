cca68416aa84906bab8a839aecbfccb9a17aa707
Mariane Malheiros <Mariane.Malheiros@libertyfrac.com>
2023-06-12 18:35:51 -0300

    veiculo model

---
+++ src/app/models/veiculo.model.ts
@@ -0,0 +1,22 @@
+import {FormControl, Validators} from "@angular/forms";
+
+export class Veiculo {
+  constructor(id: string, placa: string, chassi: string, renavam: string, modelo: string, marca: string, ano: number) {
+    this.id = id;
+    this.placa = placa;
+    this.chassi = chassi;
+    this.renavam = renavam;
+    this.modelo = modelo;
+    this.marca = marca;
+    this.ano = ano;
+
+  }
+
+  id: string;
+  placa: string;
+  chassi: string;
+  renavam: string;
+  modelo: string;
+  marca: string;
+  ano: number;
+}
