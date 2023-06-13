76326d97f9eab83626d08fc2515a2c53481e0d91
Mariane Malheiros <Mariane.Malheiros@libertyfrac.com>
2023-06-13 12:08:46 -0300

    veiculo crud form ready

--- src/app/components/add-veiculo-form/add-veiculo-form.component.ts
+++ src/app/components/add-veiculo-form/add-veiculo-form.component.ts
@@ -1,9 +1,45 @@
 import { Component } from '@angular/core';
+import { VeiculoFormService } from '../../services/veiculo-form.service';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
-  selector: 'app-add-veiculo-form',
+  selector: 'add-veiculo-form',
   templateUrl: './add-veiculo-form.component.html'
 })
 export class AddVeiculoFormComponent {
+  constructor(public form: VeiculoFormService) {
+    form.formGroup.controls.id.setValue(uuidv4())
+  }
 
+  marcas = ['Volkswagen', 'Toyota', 'Ford', 'Volvo', 'Citroen', 'Renault'];
+  modelos = new Array<string>()
+  modelosVolkswagen = ['Amarok', 'Golf', 'T-Cross', 'Tiguan', 'Up!'];
+  modelosRenault = ['Captur', 'Clio', 'Duster', 'Kwid', 'Twingo'];
+  modelosCitroen = ['C3', 'C4'];
+  modelosFord = ['EcoSport', 'Edge', 'Fiesta', 'Focus'];
+  modelosToyota = ['Corolla', 'Highlander', 'Prius', 'Sienna', 'Etios'];
+  modelosVolvo = ['C40', 'EX30', 'EX90', 'S60', 'S90'];
+
+  onMarcaChange(): void {
+    switch (this.form.formGroup.controls.marca.value) {
+      case 'Volkswagen':
+        this.modelos = this.modelosVolkswagen;
+        break;
+      case 'Toyota':
+        this.modelos = this.modelosToyota;
+        break;
+      case 'Renault':
+        this.modelos = this.modelosRenault;
+        break;
+      case 'Citroen':
+        this.modelos = this.modelosCitroen;
+        break;
+      case 'Ford':
+        this.modelos = this.modelosFord;
+        break;
+      case 'Volvo':
+        this.modelos = this.modelosVolvo;
+        break;
+    }
+  }
 }
--- src/app/services/veiculo-form.service.ts
+++ src/app/services/veiculo-form.service.ts
@@ -1,26 +1,39 @@
 import { Injectable } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { LocalStorageService } from './local-storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VeiculoFormService {
   formGroup: FormGroup<VeiculoFormGroup>;
-  constructor() {
-    this.formGroup = new FormGroup<VeiculoFormGroup>({
-      id: new FormControl('', Validators.required),
+  constructor(private localStorage: LocalStorageService) {
+    this.formGroup = new FormGroup<VeiculoFormGroup>(<VeiculoFormGroup>{
+      id: new FormControl('', { nonNullable: true, validators: Validators.required }),
       placa: new FormControl('', Validators.required),
       chassi: new FormControl('', Validators.required),
       renavam: new FormControl('', Validators.required),
       modelo: new FormControl('', Validators.required),
       marca: new FormControl('', Validators.required),
-      ano:new FormControl(1950, [Validators.required, Validators.min(1950), Validators.pattern('^[0-9]*$')]),
+      ano: new FormControl(null, [Validators.required, Validators.min(2001), Validators.pattern('^[0-9]*$')]),
     })
+
+    this.formGroup.controls.marca.valueChanges.subscribe((marcaValue) => {
+      if(marcaValue != null) {
+        this.formGroup.controls.modelo.enable();
+      } else {
+        this.formGroup.controls.modelo.disable();
+      }
+    })
+  }
+
+  submit(): void {
+    this.localStorage.set(this.formGroup.controls.id.value, this.formGroup.value);
   }
 }
 
 export type VeiculoFormGroup = {
-  id: FormControl<string | null>;
+  id: FormControl<string>;
   placa: FormControl<string | null>;
   chassi: FormControl<string | null>;
   renavam: FormControl<string | null>;
