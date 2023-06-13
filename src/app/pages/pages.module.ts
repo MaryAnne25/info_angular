6419d8efbdb033e80ec9a8af3f9f13523e9c9ff9
Mariane Malheiros <Mariane.Malheiros@libertyfrac.com>
2023-06-13 12:11:04 -0300

    adjustments

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,15 +3,21 @@ import { CommonModule } from '@angular/common';
 import { VeiculoListComponent } from './veiculo-list/veiculo-list.component';
 import { AddVeiculoComponent } from './add-veiculo/add-veiculo.component';
 import { ComponentsModule } from '../components/components.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
     VeiculoListComponent,
     AddVeiculoComponent
   ],
-  imports: [
-    CommonModule,
-    ComponentsModule
-  ]
+    imports: [
+        CommonModule,
+        ComponentsModule,
+        MatButtonModule,
+        MatIconModule,
+        TranslateModule
+    ]
 })
 export class PagesModule { }
